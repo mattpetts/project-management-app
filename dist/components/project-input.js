83a9edcaf34ebd6c91c18fa7b661dcae10bcdba9
mattpetts <mattpetts23@gmail.com>
2023-06-29 17:08:24 +0100

    split code with es2015 modules

--- dist/components/project-input.js
+++ dist/components/project-input.js
@@ -4,8 +4,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { Component } from "./base-component.js";
-import { validate } from "../util/validation.js";
+import Component from "./base-component.js";
+import * as Validation from "../util/validation.js";
 import { autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 export class ProjectInput extends Component {
@@ -44,9 +44,9 @@ export class ProjectInput extends Component {
             min: 1,
             max: 5
         };
-        if (!validate(titleValidatable) ||
-            !validate(descriptionValidatable) ||
-            !validate(peopleValidatable)) {
+        if (!Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peopleValidatable)) {
             alert('Invalid input');
             return;
         }
